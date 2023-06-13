3ac82f8975c8eeca570ee3dadeb369a1a36dde1c
Jeegar Bhalala <jeegarbhalala8@gmail.com>
2023-06-13 15:59:30 +0200

    new files added

--- day20_rangefn.py
+++ day20_rangefn.py
@@ -1,6 +1,10 @@
+print("List Generator")
+
 first_input =int( input ("Enter first input: ") )
 last_input = int(input ("Enter last input: "))
 increment =  int(input ("Enter increment amount: "))
 
 for i in range(first_input,last_input , increment):
     print(i)
+
+
